18b05f2c8438f57a52fd2eeffbc2c95e50e43ffb
anjalisadanandanp <anjalip1@iisc.ac.in>
2025-06-16 10:41:48 +0530

    updated code

--- mesageo_elephant_project/elephant_project/model/abm_model_HEC_v2.py
+++ mesageo_elephant_project/elephant_project/model/abm_model_HEC_v2.py
@@ -103,8 +103,8 @@ class Elephant(GeoAgent):
 
         #----------------hoose the type of memory matrix initialization-------------------#
         # self.initialize_food_memory_matrix_only_forest()
-        self.initialize_food_memory_matrix_random()
-        # self.initialize_food_memory_matrix_with_knowledge_from_fringe()
+        # self.initialize_food_memory_matrix_random()
+        self.initialize_food_memory_matrix_with_knowledge_from_fringe()
         #---------------------------------------------------------------------------------#
         # self.initialize_water_memory_matrix_only_forest()
         self.initialize_water_memory_matrix_forest_and_croplands()
@@ -1714,8 +1714,8 @@ class environment():
     def main(self):
 
         self.initialize_food_matrix()
-        # self.initialize_water_matrix_only_rivers()
-        self.initialize_water_matrix_only_water_holes()
+        self.initialize_water_matrix_only_rivers()
+        # self.initialize_water_matrix_only_water_holes()
 
         return
     #---------------------------------------------------------------------------------------------------------
